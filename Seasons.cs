4055e614b8b1a8ac6b3bf17473f314e00fd881db
Suden <20457922+shudnal@users.noreply.github.com>
2024-01-31 15:24:38 +0300

    hide grass in winter by default

--- Seasons.cs
+++ Seasons.cs
@@ -274,7 +274,7 @@ namespace Seasons
             seasonalStatsOutdoorsOnly = config("Season", "Seasonal stats works only outdoors", defaultValue: true, "Make seasonal stats works only outdoors");
             freezingSwimmingInWinter = config("Season", "Get freezing when swimming in cold water in winter", defaultValue: true, "Swimming in cold water during winter will get you freezing debuff");
             changeSeasonOnlyAfterSleep = config("Season", "Change season only after sleep", defaultValue: false, "Season can be changed regular way only after sleep");
-            hideGrassInWinter = config("Season", "Hide grass in winter", defaultValue: false, "Hide grass in winter");
+            hideGrassInWinter = config("Season", "Hide grass in winter", defaultValue: true, "Hide grass in winter");
             hideGrassListInWinter = config("Season", "Hide grass in set list in winter", defaultValue: "grasscross_meadows, grasscross_forest_brown, grasscross_forest, grasscross_swamp, grasscross_heath, grasscross_meadows_short, grasscross_heath_flower, grasscross_mistlands_short", "Hide set grass in winter");
 
             seasonalStatsOutdoorsOnly.SettingChanged += (sender, args) => SE_Season.UpdateSeasonStatusEffectStats();
