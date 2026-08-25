dc24993d32f9674677cd0635cb0bbf56150d5b0d
Suden <20457922+shudnal@users.noreply.github.com>
2026-08-25 03:08:21 +0300

    fix: complete Blood Moon spawn report validation imports

--- BloodMoon/BloodMoonSpawnReportGuard.cs
+++ BloodMoon/BloodMoonSpawnReportGuard.cs
@@ -2,6 +2,7 @@ using HarmonyLib;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using UnityEngine;
 using static Seasons.Seasons;
