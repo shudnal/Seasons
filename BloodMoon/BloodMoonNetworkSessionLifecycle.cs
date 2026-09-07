424a22b91b911299f886d4970a49c689d497c9c6
Suden <20457922+shudnal@users.noreply.github.com>
2026-09-07 14:22:22 +0300

    refactor: use an explicit Blood Moon network session reset

--- BloodMoon/BloodMoonNetworkSessionLifecycle.cs
+++ BloodMoon/BloodMoonNetworkSessionLifecycle.cs
@@ -1,18 +1,12 @@
 using HarmonyLib;
-using System.Reflection;
 
 namespace Seasons.BloodMoon
 {
     internal static class BloodMoonNetworkSessionLifecycle
     {
-        private static readonly FieldInfo ClientGlobalField = AccessTools.Field(typeof(BloodMoonNetwork), "<ClientGlobal>k__BackingField");
-        private static readonly FieldInfo ClientParticipantsField = AccessTools.Field(typeof(BloodMoonNetwork), "<ClientParticipants>k__BackingField");
-
         internal static void ResetClientSnapshots()
         {
-            ClientGlobalField?.SetValue(null, new BloodMoonGlobalSnapshot());
-            ClientParticipantsField?.SetValue(null, new BloodMoonParticipantSnapshot());
-            BloodMoonParticipantDetails.Reset();
+            BloodMoonNetwork.ResetSession();
         }
     }
 
