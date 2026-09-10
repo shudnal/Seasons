acdde292c50ae9b147424405b0443a31731f6296
Suden <20457922+shudnal@users.noreply.github.com>
2026-09-11 00:39:29 +0300

    Match suppressed prefab initialization with destruction

--- Utils/CustomPrefabs.cs
+++ Utils/CustomPrefabs.cs
@@ -1,5 +1,6 @@
 ﻿using HarmonyLib;
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace Seasons
 {
@@ -12,6 +13,7 @@ namespace Seasons
         private static GameObject rootPrefabs;
 
         public static bool prefabInit = false;
+        private static readonly HashSet<ItemDrop> suppressedItemDrops = new HashSet<ItemDrop>();
 
         private static void InitRootObject()
         {
@@ -76,7 +78,20 @@ namespace Seasons
         public static class ItemDrop_Awake_AddPrefab
         {
             [HarmonyPriority(Priority.First)]
-            private static bool Prefix() => !prefabInit;
+            private static bool Prefix(ItemDrop __instance)
+            {
+                if (!prefabInit)
+                    return true;
+                suppressedItemDrops.Add(__instance);
+                return false;
+            }
+        }
+
+        [HarmonyPatch(typeof(ItemDrop), nameof(ItemDrop.OnDestroy))]
+        public static class ItemDrop_OnDestroy_AddPrefab
+        {
+            [HarmonyPriority(Priority.First)]
+            private static bool Prefix(ItemDrop __instance) => !suppressedItemDrops.Remove(__instance);
         }
 
         [HarmonyPatch(typeof(ItemDrop), nameof(ItemDrop.Start))]
